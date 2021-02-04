9712495c941c9b964c7cda3e069a2e58fe117f0d
Thomas Maitre <thomas2000.maitre@gmail.com>
2021-02-04 14:29:11 -0500

    99% working but with old comment used to get speed/frequency

--- Indexa/Program.cs
+++ Indexa/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Device.Gpio;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Indexa
 {
@@ -9,144 +11,115 @@ namespace Indexa
         {
             var gpio = new GpioController();
             // readPin function
-            bool readPin(int p) => gpio.Read(p) == PinValue.High;
-
-            int cycleCounter = 0;
-            int pulseCounter = 0;
-            int state = 0;
-            double time = 0;
-            double slowestTime = 0;
-
+            //bool readPin(int p) => gpio.Read(p) == PinValue.High;
+            var path = "Log";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
             const int indexPin = 4;
             gpio.OpenPin(indexPin, PinMode.Input);
             const int pulsePin = 17;
             gpio.OpenPin(pulsePin, PinMode.Input);
 
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-
-            while (cycleCounter <= 1000)
+            for (int i = 0; i < 20; i++)
             {
-                switch (state)
+                StreamWriter sw = new StreamWriter($"{path}/ResultOfSimulation#{i}_{DateTime.Now:yyyyMMdd_HHmmss}.csv");
+
+                sw.WriteLine($"SimulationStartAt = {DateTime.Now:yyyyMMdd_HHmmss}");
+                sw.WriteLine("EachLineIsComposedOF:\nIndexCounter,Counter,SlowestSpeed");
+
+                /*StreamWriter sw2 = new StreamWriter($"{path}/DeepIndexAnalysis_{DateTime.Now:yyyyMMdd_HHmmss}.csv");
+
+                sw2.WriteLine($"SimulationTestStartAt = {DateTime.Now:yyyyMMdd_HHmmss}");
+                sw2.WriteLine("EachLineIsComposedOF:\nCounter,SlowestSpeed,Speed,State");*/
+
+                int cycleCounter = 0;
+                int pulseCounter = 0;
+                int state = 0;
+                //double time = 0;
+                //double slowestTime = 0;
+                //double[] timeTab = new double[100_000];
+                //int x = 0;
+
+                
+
+                //var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                while (cycleCounter <= 1000)
                 {
-                    case 0:
-                        {
-                            if (readPin(indexPin))
+                    switch (state)
+                    {
+                        case 0:
                             {
-                                state = 1;
-                                cycleCounter++;
+                                if ((int)gpio.Read(indexPin) == 1)
+                                {
+                                    state = 1;
+                                    cycleCounter++;
+                                }
+                                break;
                             }
-                            break;
-                        }
-                    case 1:
-                        {
-                            if (!readPin(indexPin))
+                        case 1:
                             {
-                                state = 2;
+                                if ((int)gpio.Read(indexPin) == 0)
+                                {
+                                    state = 2;
+                                }
+                                break;
                             }
-                            break;
-                        }
-                    case 2:
-                        {
-                            if (readPin(indexPin))
+                        case 2:
                             {
-                                state = 1;
-                                cycleCounter++;
-                                Console.WriteLine($"Index : {cycleCounter} | pulse : {pulseCounter} | fréquence la plus lente obtenue dans ce cycle (kHz) : {1 / slowestTime}");
-                                pulseCounter = 0;
+                                if ((int)gpio.Read(indexPin) == 1)
+                                {
+                                    state = 1;
+                                    cycleCounter++;
+                                    //Console.WriteLine($"Index : {cycleCounter} | pulse : {pulseCounter} | fréquence la plus lente obtenue dans ce cycle (kHz) : {1 / slowestTime}");
+                                    sw.WriteLineAsync($"{cycleCounter}, {pulseCounter}");
+                                    //slowestTime = 0;
+                                    pulseCounter = 0;
+                                }
+                                else if ((int)gpio.Read(pulsePin) == 1)
+                                {
+                                    state = 3;
+                                    pulseCounter++;
+                                }
+                                break;
                             }
-                            else if (readPin(pulsePin))
+                        case 3:
                             {
-                                state = 3;
-                                pulseCounter++;
+                                if ((int)gpio.Read(indexPin) == 1)
+                                {
+                                    state = 1;
+                                    cycleCounter++;
+                                    //Console.WriteLine($"Index : {cycleCounter} | pulse : {pulseCounter} | fréquence la plus lente obtenue dans ce cycle (kHz) : {0:000.00}", (1 / slowestTime));
+                                    sw.WriteLineAsync($"{cycleCounter}, {pulseCounter}");
+                                    //slowestTime = 0;
+                                    pulseCounter = 0;
+                                }
+                                else if ((int)gpio.Read(pulsePin) == 0)
+                                {
+                                    state = 2;
+                                }
+                                break;
                             }
-                            break;
-                        }
-                    case 3:
-                        {
-                            if (readPin(indexPin))
-                            {
-                                state = 1;
-                                cycleCounter++;
-                                Console.WriteLine($"Index : {cycleCounter} | pulse : {pulseCounter} | fréquence la plus lente obtenue dans ce cycle (kHz) : {0:000.00}", (1 / slowestTime));
-                                pulseCounter = 0;
-                            }
-                            else if (!readPin(pulsePin))
-                            {
-                                state = 2;
-                            }
-                            break;
-                        }
+                    }
+                    /*if (time > slowestTime)
+                    {
+                        slowestTime = time;
+                        //Console.WriteLine($"{1 / slowestTime / 1000}");
+                    }
+                    //saving elapsed time
+                    time = watch.Elapsed.TotalMilliseconds;
+                    sw2.WriteLine($"{cycleCounter}, {pulseCounter}, {state}, {time}");
+                    watch.Stop();
+                    watch = System.Diagnostics.Stopwatch.StartNew();*/
                 }
-                if (time > slowestTime)
-                {
-                    slowestTime = time;
-                    Console.WriteLine($"{1 / slowestTime / 1000}");
-                }
-                //saving elapsed time
-                time = watch.Elapsed.TotalMilliseconds;
-                watch.Stop();
-                watch = System.Diagnostics.Stopwatch.StartNew();
+                sw.WriteLine($"SimulationEndedAt = {DateTime.Now:yyyyMMdd_HHmmss}");
+                sw.Close();
+                /*sw2.WriteLine($"SimulationEndedAt = {DateTime.Now:yyyyMMdd_HHmmss}");
+                sw2.Close();*/
+                Console.WriteLine($"testEnded#{i}");
             }
-            
-            
-            
-            /*
-            while(cycleCounter <= 50)
-            {
-                switch (indexState)
-                {
-                    case 0:
-                        {
-                            if (readPin(indexPin)) //if indexPin == 1
-                            {
-                                indexState = 1;
-                                ++cycleCounter;
-                                while (!readPin(indexPin)) //while indexPin == 0
-                                {
-                                    var watch = System.Diagnostics.Stopwatch.StartNew();
-                                    
-
-                                    if (time > slowestTime)
-                                    {
-                                        slowestTime = time;
-                                        //Console.WriteLine($"{1 / slowestTime / 1000}");
-                                    }
-                                    //saving elapsed time
-                                    time = watch.Elapsed.TotalMilliseconds;
-                                    //saving elapsed time in array
-                                    timeTab[pulseCounter] = time;
-                                    watch.Stop();
-                                    watch = System.Diagnostics.Stopwatch.StartNew();
-                                }
-
-                                for (int y = 0; y < pulseCounter; y++)
-                                {
-                                    moy += timeTab[y];
-                                }
-                                moy /= pulseCounter;
-
-
-
-                                Console.WriteLine($"Temps pour lire {cycleNum} index: "
-                                                    + (stopWatch.Elapsed.TotalMilliseconds).ToString("#,##0.000", new CultureInfo("fr-CA"))
-                                                    + " ms" + $" | temps moyen : {moy} ms | fréquence la plus lente obtenue dans ce cycle : {1 / slowestTime}");
-                                cycleCounter = 0;
-                                pulseCounter = 0;
-                                stopWatch.Restart();
-                                indexState = 0;
-                            }
-                            break;
-                        }
-                    case 1:
-                        {
-                            if (!readPin(pulsePin))
-                            {
-                                indexState = 0;
-                            }
-                            break;
-                        }
-                }
-            }*/
         }
     }
 }
